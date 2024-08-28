3916f4b7c9249ee48ddf9db3fc55f18f198fa639
androsgithub <andreemaprinc@gmail.com>
2024-08-28 20:01:26 -0300

    feat: add fvp configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:audio_player/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:fvp/fvp.dart' as fvp;
 
 void main() {
+  fvp.registerWith();
   runApp(const MyApp());
 }
