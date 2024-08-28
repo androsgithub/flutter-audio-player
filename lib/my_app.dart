08f8d50c64be17beba791bfe727b0d7c9eb11c8e
androsgithub <andreemaprinc@gmail.com>
2024-08-28 19:57:15 -0300

    feat: create separated file to MyApp widget

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:audio_player/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
