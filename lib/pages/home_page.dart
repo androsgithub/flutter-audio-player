023342d752523d96a033ddd84a5484ca21f062ad
androsgithub <andreemaprinc@gmail.com>
2024-08-31 00:02:24 -0300

    feat: add theme configs, change some alertdialog widgets, and refactor to phone devices

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,9 @@ import 'package:audio_player/shared/widgets/title_custom.dart';
 import 'package:audio_player/widgets/player_widget.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
+import 'dart:io' show Platform;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -29,12 +31,24 @@ class _HomePageState extends State<HomePage> {
   //inicializar
 
   _pegarPaths() async {
+    paths = [];
     paths = await appSettingsRepository.getPaths();
-    setState(() {
+
+    playerService.playlist = [];
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
       for (var path in paths) {
-        playerService.playlist.addAll(songsRepository.getSongs(path));
+        playerService.playlist
+            .addAll(await songsRepository.getSongs(path: path));
       }
-    });
+    } else {
+      playerService.playlist.addAll(await songsRepository.getSongs());
+    }
+    if (playerService.playlist.isNotEmpty) {
+      playerService
+          .setSong(playerService.playlist[playerService.songIndex].data);
+    }
+
+    setState(() {});
   }
 
   _inicializar() async {
@@ -57,103 +71,128 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     playerService = Provider.of<PlayerService>(context);
     return Scaffold(
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 350),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: openPlaylist, icon: const Icon(Icons.list)),
-                  IconButton(
-                      onPressed: openConfig, icon: const Icon(Icons.settings))
-                ],
-              ),
-              playerService.playlist.isEmpty
-                  ? const Center(child: Text("Nenhuma musica selecionada"))
-                  : Column(
-                      children: [
-                        AspectRatio(
-                            aspectRatio: 1,
-                            child: playerService
-                                        .playlist[playerService.songIndex]
-                                        .songImage ==
-                                    ""
-                                ? const Icon(
-                                    Icons.play_circle_rounded,
-                                    size: 256,
-                                  )
-                                : Image.network(
-                                    playerService
-                                        .playlist[playerService.songIndex]
-                                        .songImage,
-                                    fit: BoxFit.cover,
-                                  )),
-                        const SizedBox(height: 5),
-                        PlayerWidget(
-                            player: playerService.player,
-                            songName: playerService
-                                .playlist[playerService.songIndex].songName,
-                            artistName: playerService
-                                .playlist[playerService.songIndex].artistName,
-                            nextSong: playerService.nextSong,
-                            previousSong: playerService.previousSong,
-                            changeRepeatMode: playerService.changeRepeatType,
-                            repeatMode: playerService.repeatType),
-                      ],
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: openPlaylist, icon: const Icon(Icons.list)),
+          IconButton(onPressed: openConfig, icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        child: Center(
+          child: playerService.playlist.isEmpty
+              ? const Text("Nenhuma musica selecionada")
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        constraints: const BoxConstraints(maxWidth: 250),
+                        child: const FittedBox(
+                          child: Icon(
+                            Icons.play_circle_rounded,
+                          ),
+                        ),
+                      ),
                     ),
-            ],
-          ),
+                    PlayerWidget(
+                        player: playerService.player,
+                        songName: playerService
+                            .playlist[playerService.songIndex].title,
+                        artistName: playerService
+                            .playlist[playerService.songIndex].artist,
+                        nextSong: playerService.nextSong,
+                        previousSong: playerService.previousSong,
+                        changeRepeatMode: playerService.changeRepeatType,
+                        repeatMode: playerService.repeatType),
+                  ],
+                ),
         ),
       ),
     );
   }
 
+  //Playlist
   openPlaylist() {
+    var playlistFiltered = playerService.playlist;
+
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
+                backgroundColor: Theme.of(context).colorScheme.primary,
                 shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(2))),
+                    borderRadius: BorderRadius.all(Radius.circular(16))),
                 title: TitleCustom(
                     text: "Playlist - ${playerService.playlist.length} files"),
                 content: StatefulBuilder(
-                  builder: (context, customSetState) => Container(
-                    constraints: const BoxConstraints(maxWidth: 350),
-                    width: double.maxFinite,
-                    child: ListView.builder(
-                      itemCount: playerService.playlist.length,
-                      itemBuilder: (context, index) {
-                        var song = playerService.playlist[index];
-                        return ListTile(
-                          selected: playerService.songIndex == index,
-                          title: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  song.songName,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
+                  builder: (context, customSetState) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        onChanged: (value) {
+                          customSetState(() {
+                            if (value.isEmpty) {
+                              playlistFiltered = playerService.playlist;
+                            } else {
+                              playlistFiltered = playerService.playlist
+                                  .where((song) =>
+                                      song.title
+                                          .toString()
+                                          .toLowerCase()
+                                          .contains(value) ||
+                                      song.artist
+                                          .toString()
+                                          .toLowerCase()
+                                          .contains(value) ||
+                                      song.data
+                                          .toString()
+                                          .toLowerCase()
+                                          .contains(value))
+                                  .toList();
+                            }
+                          });
+                        },
+                        decoration:
+                            const InputDecoration(border: OutlineInputBorder()),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: double.maxFinite,
+                          height: double.maxFinite,
+                          child: ListView.builder(
+                            itemCount: playlistFiltered.length,
+                            itemBuilder: (context, index) {
+                              var song = playlistFiltered[index];
+                              return ListTile(
+                                key: ValueKey(index),
+                                selected: playerService.songIndex == index,
+                                title: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        song.title,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    IconButton(
+                                        onPressed: () {
+                                          customSetState(() {
+                                            playerService.songIndex = index;
+                                            playerService.setSong(song.data);
+                                          });
+                                        },
+                                        icon: const Icon(Icons.play_arrow))
+                                  ],
                                 ),
-                              ),
-                              IconButton(
-                                  onPressed: () {
-                                    customSetState(() {
-                                      playerService.songIndex = index;
-                                      playerService.setSong(song.songPath);
-                                    });
-                                  },
-                                  icon: const Icon(Icons.play_arrow))
-                            ],
+                              );
+                            },
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 actions: [
@@ -172,6 +211,7 @@ class _HomePageState extends State<HomePage> {
       builder: (context) {
         return StatefulBuilder(
           builder: (context, setState) => AlertDialog(
+            backgroundColor: Theme.of(context).colorScheme.primary,
             shape: const RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(Radius.circular(2))),
             title: const TitleCustom(text: "Configuracoes"),
@@ -188,6 +228,8 @@ class _HomePageState extends State<HomePage> {
                       Expanded(
                         child: Slider(
                             value: playerService.player.volume,
+                            thumbColor: Theme.of(context).colorScheme.tertiary,
+                            activeColor: Theme.of(context).colorScheme.tertiary,
                             onChanged: (value) async {
                               await playerService.player.setVolume(value);
                               setState(() {});
@@ -196,70 +238,101 @@ class _HomePageState extends State<HomePage> {
                       Text("${(playerService.player.volume * 100).round()}%")
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const LabelCustom(text: "Diretorios"),
-                          const SizedBox(width: 10),
-                          TextButton(
-                            onPressed: () {
-                              limpar() {
-                                setState(() {
-                                  paths = [];
-                                  appSettingsRepository.setPaths([]);
-                                  playerService.playlist = [];
-                                  Navigator.pop(context);
-                                });
+                  Visibility(
+                    visible: Platform.isWindows ||
+                        Platform.isLinux ||
+                        Platform.isMacOS,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const LabelCustom(text: "Diretorios"),
+                            const SizedBox(width: 10),
+                            TextButton(
+                              onPressed: () {
+                                limpar() {
+                                  setState(() {
+                                    paths = [];
+                                    appSettingsRepository.setPaths([]);
+                                    playerService.playlist = [];
+                                    Navigator.pop(context);
+                                  });
+                                }
+
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text("Deseja mesmo limpar?"),
+                                    content: const Text(
+                                        "Você ira perder todas as suas musicas!"),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text("Cancelar")),
+                                      TextButton(
+                                          onPressed: limpar,
+                                          child: const Text("Confirmar"))
+                                    ],
+                                  ),
+                                );
+                              },
+                              child: const Text("Limpar"),
+                            )
+                          ],
+                        ),
+                        IconButton(
+                            onPressed: () async {
+                              String? selectedDirectory =
+                                  await FilePicker.platform.getDirectoryPath();
+                              if (selectedDirectory == null) {
+                                return;
                               }
 
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: const Text("Deseja mesmo limpar?"),
-                                  content: const Text(
-                                      "Você ira perder todas as suas musicas!"),
-                                  actions: [
-                                    TextButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: const Text("Cancelar")),
-                                    TextButton(
-                                        onPressed: limpar,
-                                        child: const Text("Confirmar"))
-                                  ],
-                                ),
-                              );
+                              setState(() {
+                                paths.add(selectedDirectory);
+                              });
                             },
-                            child: const Text("Limpar"),
-                          )
-                        ],
-                      ),
-                      IconButton(
-                          onPressed: () async {
-                            String? selectedDirectory =
-                                await FilePicker.platform.getDirectoryPath();
-                            if (selectedDirectory == null) {
-                              return;
-                            }
-
-                            setState(() {
-                              paths.add(selectedDirectory);
-                            });
-                          },
-                          icon: const Icon(Icons.add)),
-                    ],
+                            icon: const Icon(Icons.add)),
+                      ],
+                    ),
                   ),
-                  SizedBox(
+                  Container(
+                    clipBehavior: Clip.hardEdge, // this
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0, color: Colors.transparent),
+                    ),
+
                     width: double.maxFinite,
                     child: ListView.builder(
                       shrinkWrap: true,
                       itemCount: paths.length,
                       itemBuilder: (context, index) {
                         var path = paths[index];
-                        return ListTile(title: Text(path));
+                        return Slidable(
+                          startActionPane: ActionPane(
+                              extentRatio: 0.4,
+                              motion: const DrawerMotion(),
+                              children: [
+                                SlidableAction(
+                                  label: "Remover",
+                                  backgroundColor: Colors.red,
+                                  onPressed: (context) {
+                                    setState(() {
+                                      paths = paths
+                                          .where((path_) => path_ != path)
+                                          .toList();
+                                    });
+                                  },
+                                  icon: Icons.delete,
+                                )
+                              ]),
+                          child: ListTile(
+                            title: Text(path),
+                          ),
+                        );
                       },
                     ),
                   ),
