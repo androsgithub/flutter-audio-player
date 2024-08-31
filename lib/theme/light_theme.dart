453ca4734e0016fdacf8c8ac9b7d69a5a53bcc46
androsgithub <andreemaprinc@gmail.com>
2024-08-31 00:06:45 -0300

    fix: fix the surface color

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-      surface: Colors.black,
+      surface: Colors.white,
       primary: Colors.grey[100]!,
       secondary: Colors.grey[200]!,
       tertiary: Colors.grey[400]),
