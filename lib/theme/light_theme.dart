b2671deda66854106d7dcd79deef20059fd97876
androsgithub <andreemaprinc@gmail.com>
2024-08-31 00:04:08 -0300

    feat: create a separed files to set the light and dark theme

---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+      surface: Colors.black,
+      primary: Colors.grey[100]!,
+      secondary: Colors.grey[200]!,
+      tertiary: Colors.grey[400]),
+);
